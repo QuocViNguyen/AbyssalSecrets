177d2fcfdb6ed6b06f4977229315d74812e65e99
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-04-24 12:55:41 -0500

    Sprinting added

--- Abyssal Secrets/Assets/Sprite Assets/Player/player_movement.cs
+++ Abyssal Secrets/Assets/Sprite Assets/Player/player_movement.cs
@@ -6,15 +6,13 @@ public class player_movement : MonoBehaviour
 {
     public float movementSpeed = 30;
     public float diveSpeed = 30;
-    private bool facingRight;
-    private bool isFlipped;
+    private bool isFlippedX;
     private bool isIdle;
     [SerializeField] Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        facingRight = true;
-        isFlipped = false;
+        isFlippedX = false;
         isIdle = true;
     }
 
@@ -24,34 +22,44 @@ public class player_movement : MonoBehaviour
         var movementHorizontal = Input.GetAxis("Horizontal");
         var movementVertical = Input.GetAxis("Vertical");
 
-        if (movementHorizontal < 0 && !isFlipped)
+        if (movementHorizontal < 0 && !isFlippedX)
         {
             Flip();
         }
-        else if (movementHorizontal > 0 && isFlipped)
+        else if (movementHorizontal > 0 && isFlippedX)
         {
             Flip();
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isShift", true);
+            movementSpeed = 60;
+        }
+        else
+        {
+            animator.SetBool("isShift", false);
+            movementSpeed = 30;
         }
 
         CheckIdle(movementHorizontal, movementVertical);
         animator.SetFloat("swimming", Mathf.Abs(movementHorizontal));
         transform.position += new Vector3(movementHorizontal, 0, 0) * Time.deltaTime * movementSpeed;
         transform.position += new Vector3(0, movementVertical, 0) * Time.deltaTime * diveSpeed;
-
-        Debug.Log(movementHorizontal);
-        
     }
 
     private void Flip()
     {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
-
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
+        if (theScale.y < 0)
+        {
+            theScale.y*= -1;
+        }
         transform.localScale = theScale;
-        isFlipped = !isFlipped;
+
+        isFlippedX = !isFlippedX;
     }
 
     private void CheckIdle(float movementHorizontal, float movementVertical)
