b048be560deba9a922853213ef33e33f74c20a63
Thai Nguyen <nqcuong2@gmail.com>
2021-04-24 20:17:44 -0500

    Add store to buy items

--- Abyssal Secrets/Assets/Scripts/Item.cs
+++ Abyssal Secrets/Assets/Scripts/Item.cs
@@ -4,34 +4,29 @@ using UnityEngine;
 
 public abstract class Item
 {
-    protected int price;
-    protected string description;
-    protected Sprite itemSprite;
+    public Sprite ItemSprite { get; private set; }
+
+    protected Item(Sprite sprite)
+    {
+        ItemSprite = sprite;
+    }
 
     public abstract void Use();
 }
 
 public class SpeedBoost : Item
 {
-    public SpeedBoost()
-    {
-        price = 10;
-        description = "This item boosts your diving speed significantly in a short period of time";
-    }
+    public SpeedBoost(Sprite sprite) : base(sprite) { }
 
     public override void Use()
     {
-
+        Debug.Log("SpeedBoost used");
     }
 }
 
 public class Shield : Item
 {
-    public Shield()
-    {
-        price = 25;
-        description = "This item protects you from attack of all creatures";
-    }
+    public Shield(Sprite sprite) : base(sprite) { }
 
     public override void Use()
     {
@@ -39,30 +34,32 @@ public class Shield : Item
     }
 }
 
-public class InkBomb : Item
+public class SmellyBomb : Item
 {
-    public InkBomb()
-    {
-        price = 15;
-        description = "This item prevents creatures from seeing you";
-    }
+    public SmellyBomb(Sprite sprite) : base(sprite) { }
 
     public override void Use()
     {
-        
+        Debug.Log("SmellyBomb used");
     }
 }
 
 public class Meat : Item
 {
-    public Meat()
-    {
-        price = 15;
-        description = "This piece of meat saves you from hungry creatures";
-    }
+    public Meat(Sprite sprite) : base(sprite) { }
 
     public override void Use()
     {
         Object.Instantiate(Resources.Load("Meat"));
+    }
+}
+
+public class Rune : Item
+{
+    public Rune(Sprite sprite) : base(sprite) { }
+
+    public override void Use()
+    {
+        Debug.Log("Rune used");
     }
 }
--- Abyssal Secrets/Assets/Scripts/ItemManager.cs
+++ Abyssal Secrets/Assets/Scripts/ItemManager.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
+    [SerializeField] Image[] itemImages;
+
     private const int MaxItemSlots = 5;
     private Item[] items = new Item[MaxItemSlots];
     private int slotCount = 0;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        items[0] = new Meat();
-        items[1] = new Shield();
+        foreach (Image itemImage in itemImages)
+            itemImage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,35 +22,46 @@ public class ItemManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            items[0].Use();
+            UseItem(0);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            items[1].Use();
+            UseItem(1);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            items[2].Use();
+            UseItem(2);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            items[3].Use();
+            UseItem(3);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
-            items[4].Use();
+            UseItem(4);
         }
     }
 
-    public void AddItem(Item item)
+    private void UseItem(int slotIndex)
+    {
+        if (slotIndex < slotCount && itemImages[slotIndex].gameObject.activeSelf)
+        {
+            items[slotIndex].Use();
+            itemImages[slotIndex].gameObject.SetActive(false);
+        }
+    }
+
+    public bool AddItem(Item item)
     {
         if (slotCount < MaxItemSlots)
         {
-            items[slotCount++] = item;
+            items[slotCount] = item;
+            itemImages[slotCount].sprite = item.ItemSprite;
+            itemImages[slotCount++].gameObject.SetActive(true);
+            return true;
         }
-        else
-        {
-            Debug.Log("Already full slots");
-        }
+
+        Debug.Log("Already full slots");
+        return false;
     }
 }
